1255fcf21fb684e1c7bb5689265e17b038adcd3d
amythompson9 <amythompson9@gmail.com>
2017-12-19 19:49:50 -0500

    Added Controllers

---
+++ Stemma/Controllers/CareersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Stemma.Models;
+
+namespace Stemma.Controllers
+{
+    public class CareersController : Controller
+    {
+        private STEMMAEntities db = new STEMMAEntities();
+
+        // GET: Careers
+        public ActionResult Index()
+        {
+            return View(db.Careers.ToList());
+        }
+
+        // GET: Careers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Career career = db.Careers.Find(id);
+            if (career == null)
+            {
+                return HttpNotFound();
+            }
+            return View(career);
+        }
+
+        // GET: Careers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Careers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CareerID,AttendedCollege,Graduations,HaveJob,HadJob,Accomplishments,FirstDayOfSchool,ArmedForces,Volunteer,Legacies,TechSchool,LicensesCertifications,FirstJob,WorstJob")] Career career)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Careers.Add(career);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(career);
+        }
+
+        // GET: Careers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Career career = db.Careers.Find(id);
+            if (career == null)
+            {
+                return HttpNotFound();
+            }
+            return View(career);
+        }
+
+        // POST: Careers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CareerID,AttendedCollege,Graduations,HaveJob,HadJob,Accomplishments,FirstDayOfSchool,ArmedForces,Volunteer,Legacies,TechSchool,LicensesCertifications,FirstJob,WorstJob")] Career career)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(career).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(career);
+        }
+
+        // GET: Careers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Career career = db.Careers.Find(id);
+            if (career == null)
+            {
+                return HttpNotFound();
+            }
+            return View(career);
+        }
+
+        // POST: Careers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Career career = db.Careers.Find(id);
+            db.Careers.Remove(career);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Stemma/Controllers/EventTablesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Stemma.Models;
+
+namespace Stemma.Controllers
+{
+    public class EventTablesController : Controller
+    {
+        private STEMMAEntities db = new STEMMAEntities();
+
+        // GET: EventTables
+        public ActionResult Index()
+        {
+            var eventTables = db.EventTables.Include(e => e.EventDate);
+            return View(eventTables.ToList());
+        }
+
+        // GET: EventTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventTable eventTable = db.EventTables.Find(id);
+            if (eventTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventTable);
+        }
+
+        // GET: EventTables/Create
+        public ActionResult Create()
+        {
+            ViewBag.EventDateID = new SelectList(db.EventDates, "EventDateID", "Month");
+            return View();
+        }
+
+        // POST: EventTables/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EventID,DateCreated,EventTitle,EventDateID,PeopleInvolved,EventCaption")] EventTable eventTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EventTables.Add(eventTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EventDateID = new SelectList(db.EventDates, "EventDateID", "Month", eventTable.EventDateID);
+            return View(eventTable);
+        }
+
+        // GET: EventTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventTable eventTable = db.EventTables.Find(id);
+            if (eventTable == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EventDateID = new SelectList(db.EventDates, "EventDateID", "Month", eventTable.EventDateID);
+            return View(eventTable);
+        }
+
+        // POST: EventTables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EventID,DateCreated,EventTitle,EventDateID,PeopleInvolved,EventCaption")] EventTable eventTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eventTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EventDateID = new SelectList(db.EventDates, "EventDateID", "Month", eventTable.EventDateID);
+            return View(eventTable);
+        }
+
+        // GET: EventTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventTable eventTable = db.EventTables.Find(id);
+            if (eventTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventTable);
+        }
+
+        // POST: EventTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EventTable eventTable = db.EventTables.Find(id);
+            db.EventTables.Remove(eventTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
