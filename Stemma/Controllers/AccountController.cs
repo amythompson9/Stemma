28570dcd7d11d48372a769373f2312c18dad599b
staceyspikesgit config --global user.name staceyspikes <stacey.lynne13@yahoo.com>
2018-01-02 20:22:22 -0500

    fixed authentication

--- Stemma/Controllers/AccountController.cs
+++ Stemma/Controllers/AccountController.cs
@@ -329,7 +329,7 @@ namespace Stemma.Controllers
             }
 
             // Sign in the user with this external login provider if the user already has a login
-            var result = await SignInManager.ExternalSignInAsync(loginInfo, isPersistent: false);
+            var result = await SignInManager.ExternalSignInAsync(loginInfo, isPersistent: true);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -337,7 +337,7 @@ namespace Stemma.Controllers
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = false });
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = true });
                 case SignInStatus.Failure:
                 default:
                     // If the user does not have an account, then prompt the user to create an account
