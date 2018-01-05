f9813df007657377591b3117bd6e658f70b2be84
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-01-05 11:46:36 -0500

    fixed db script. was asian somehow, now it's latin again

--- Stemma/Controllers/EventTablesController.cs
+++ Stemma/Controllers/EventTablesController.cs
@@ -17,7 +17,39 @@ namespace Stemma.Controllers
         // GET: EventTables
         public ActionResult Index()
         {
+            //you are trying to add a sorting feature :(
+
+            var queryWholeTimeline = from EventTable in db.EventTables
+            orderby
+              EventTable.EventYear,
+              EventTable.EventMonth,
+              EventTable.EventDay descending
+            select new
+            {
+                EventID = EventTable.EventID,
+                DateCreated = EventTable.DateCreated,
+                EventTitle = EventTable.EventTitle,
+                EventDay = EventTable.EventDay,
+                EventMonth = EventTable.EventMonth,
+                EventYear = EventTable.EventYear,
+                PeopleInvolved = EventTable.PeopleInvolved,
+                EventCaption = EventTable.EventCaption
+            };
+
+            //foreach (var timePoint in queryWholeTimeline)
+            //{
+                //something
+            //}
+
+            //this was the only original line in this code:
             return View(db.EventTables.ToList());
+
+            //try changing it to:
+            //return View(queryWholeTimeline.ToList());
+            //return View(queryWholeTimeline);
+            //or not lol
+
+
         }
 
         // GET: EventTables/Details/5
