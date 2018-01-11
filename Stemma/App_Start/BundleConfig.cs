83e3fc9d1d87a0f427e8bbcf7d1515b2ce66fbf1
Holly N Little <hollyisalwayshopeful@gmail.com>
2018-01-10 20:04:17 -0500

    added leafstyles.css

--- Stemma/App_Start/BundleConfig.cs
+++ Stemma/App_Start/BundleConfig.cs
@@ -26,7 +26,9 @@ namespace Stemma
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/LeafStyles.css"
+                ));
         }
     }
 }
